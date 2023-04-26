a27f69147c3becf57864af0aaec3673009611905
maxinimize <maxinimize@gmail.com>
2023-04-26 10:06:37 +0800

    collisionDetection test updated

--- tests/collisionDetection.sh
+++ tests/collisionDetection.sh
@@ -4,5 +4,6 @@ emcc collisionDetection.c \
   -s MODULARIZE=1 \
   -s DEMANGLE_SUPPORT=1 \
   -s ALLOW_MEMORY_GROWTH=1 \
-  -s "EXPORTED_FUNCTIONS=['_collisionDetection']" \
+  -s "EXPORTED_FUNCTIONS=['_collisionDetection', '_malloc', '_free']" \
+  -s EXPORTED_RUNTIME_METHODS=cwrap  \
   -o collisionDetection.js
